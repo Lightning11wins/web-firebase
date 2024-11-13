07d0ca9175d4a4459f5d68ddce8ed0e0c674cbfb
Lightning <fulle637@umn.edu>
2024-11-13 17:07:38 -0600

    action help aw?

--- app/controllers/testing.js
+++ app/controllers/testing.js
@@ -1,6 +1,15 @@
 import Controller from '@ember/controller';
 import { service } from '@ember/service';
+import { GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
 
 export default class TestingController extends Controller {
   @service firebase;
+  @service auth;
+
+  @action test() {
+    const auth = getAuth(this.firebase.app);
+    console.log(auth.currentUser);
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  }
 }
--- app/routes/testing.js
+++ app/routes/testing.js
@@ -1,3 +1,13 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+import { getAuth } from 'firebase/auth';
 
-export default class TestingRoute extends Route {}
+export default class TestingRoute extends Route {
+    @service firebase;
+    
+    async model() {
+        const auth = getAuth(this.firebase.app);
+        await auth.authStateReady();
+        return auth.currentUser;
+    }
+}
